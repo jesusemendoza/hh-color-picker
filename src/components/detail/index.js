e06dd05203aeb3d1cb7c6336cb88be0fac89305f
Jesus Mendoza <je.mendoza@live.com>
2018-06-11 14:45:02 -0700

    added a basic detail component

---
+++ src/components/detail/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './_detail.scss'
+
+export default class Detail extends React.Component {
+
+    render(){
+        return (
+            <div
+            className='detail-container' 
+            style={{width:'80%', height: '60%', background:'red', right:'2%', top:'10%' }
+            }>
+            </div>
+        )
+    }
+}
